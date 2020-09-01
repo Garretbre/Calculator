e3a7cf6de8ad5140c79e818725109e45210d129d
Garretbre <67977807+Garretbre@users.noreply.github.com>
2020-09-01 14:10:34 -0700

    Working Calculator

--- Calaculator/Calaculator/JavaScript/Calculator_JavaScript.js
+++ Calaculator/Calaculator/JavaScript/Calculator_JavaScript.js
@@ -1,100 +1,100 @@
 //creates an object to keep track of values
-const Calculator = {
+const calculator = {
 // this is displays 0 on the screen
-Display_Value: '0',
+displayValue: '0',
 // this will hold the first operand for any expressions, we set it to null for now
-First_Operand: null,
+firstOperand: null,
 // this checks whether or not the second operand has been input
-Wai_Second_Operand: false,
+waitSecondOperand: false,
 // this will hold the operator, we set it to null for now
 operator: null,
 };
 
 // this modifies values each time a button is clicked
-function Input_Digit(digit) {
-    const { Display_Value, Wait_Second_Operand } = Calculator;
+function inputDigit(digit) {
+    const { displayValue, waitSecondOperand } = calculator;
     // we are checking to see if Wait_Second_Operand is true and set
     // Display_Value to the key that was clicked.
-    if (Wait_Second_Operand === true) {
-        Calculator.Display_Value = digit;
-        Calculator.Wait_Second_Operand = false;
+    if (waitSecondOperand === true) {
+        calculator.displayValue = digit;
+        calculator.waitSecondOperand = false;
     } else {
     // this overwrite Display_Value if the current value is 0
     // otherwise it add onto it
-    Calculator.Display_Value = Display_Value === '0' ? digit : Display_Value + digit;
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
     }
 }
 // this section handles decimal points
-function Input_Decimal(dot) {
+function inputDecimal(dot) {
     // this ensures that accidental clicking of the decimal point
     // doesn't cause bugs in your operation
-    if (Calculator.Wait_Second_Operand === true) return;
-    if (!Calculator.Display_Value.includes(dot)) {
+    if (calculator.waitSecondOperand === true) return;
+    if (!calculator.displayValue.includes(dot)) {
     // we are saying that if the Display_Value does not contain a decimal point
     // we want to add a decimal point
-    Calculator.Display_Value += dot;
+    calculator.displayValue += dot;
     }
 }
 
 //this section handles operators
-function Handle_Operator(Next_Operator) {
-    const { First_Operand, Display_Value, operator } = Calculator
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
     //when an operator key is pressed, we convert the current number
     //displayed on the screen to a number and then store the result in
     //Calculator.First_Operand if it doesn't already exist
-    const Value_of_Input = parseFloat(Display_Value);
+    const valueOfInput = parseFloat(displayValue);
     //checks if an operator already exists and if Wait_Second_Operand is true,
     //then updates the operator and exits from the function
-    if (operator && Calculator.Wait_Second_Operator) {
-        Calculator.operator = Next_Operator;
+    if (operator && calculator.waitSecondOperand) {
+        calculator.operator = nextOperator;
         return;
     }
-    if (First_Operand == null) {
-        Calculator.First_Operand = Value_of_Input;
+    if (firstOperand == null) {
+        calculator.firstOperand = valueOfInput;
     } else if (operator) {//checks if an operator already exists
-    const Value_Now = First_Operand || 0;
+    const valueNow = firstOperand || 0;
     //if operator exists, property lookup is performed for the operator
     //in the Perform_Calculation object and the function that matches the
     //operator is executed
-    let result = Perform_Calculation[operator](Value_Now, Value_of_Input);
+    let result = performCalculation[operator](valueNow, valueOfInput);
     //here we add a fixed amount of numbers after the decimal
     result = Number(result).toFixed(9)
     //this will remove any trailing 0's
     result = (result * 1).toString()
-    Calculator.Display_Value = parseFloat(result);
-    Calculator.First_Operand = parseFloat(result);
+    calculator.displayValue = parseFloat(result);
+    calculator.firstOperand = parseFloat(result);
     }
-    Calculator.Wait_Second_Operand = true;
-    Calculator.operator = Next_Operator;
+    calculator.waitSecondOperand = true;
+    calculator.operator = nextOperator;
 }
 
-const Perform_Calculation = {
-    '/': (First_Operand, Second_Operand) => First_Operand / Second_Operand,
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
 
-    '*': (First_Operand, Second_Operand) => First_Operand * Second_Operand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
 
-    '+': (First_Operand, Second_Operand) => First_Operand + Second_Operand,
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
 
-    '-': (First_Operand, Second_Operand) => First_Operand - Second_Operand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
 
-    '=': (First_Operand, Second_Operand) => Second_Operand 
+    '=': (firstOperand, secondOperand) => secondOperand 
 };
 
-function Calculator_Reset() {
-    Calculator.Display_Value = '0';
-    Calculator.First_Operand = null;
-    Calculator.Wait_Second_Operand = false;
-    Calculator.operator = null;
+function calculatorReset() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitSecondOperand = false;
+    calculator.operator = null;
 }
 // this function updates the screen with the contents of Display_Value
-function Update_Display() {
+function updateDisplay() {
     const display = document.querySelector('.calculator-screen');
-    display.value = Calculator.Display_Value;
+    display.value = calculator.displayValue;
 }
 
-Update_Display();
+updateDisplay();
 // this section monitors button clicks
-const keys = document.querySelector('.calculator_keys');
+const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
     //the terget variable is an object that represents the element
     // that was clicked
@@ -105,23 +105,23 @@ keys.addEventListener('click', (event) => {
     }
 
 if (target.classList.contains('operator')) {
-    Handle_Operator(target.value);
-    Update_Display();
+    handleOperator(target.value);
+    updateDisplay();
     return;
 }
 
 if (target.classList.contains('decimal')) {
-    Input_Decimal(target.value);
-    Update_Display();
+    inputDecimal(target.value);
+    updateDisplay();
     return;
 }
 // ensures that AC clears the numbers from the Calculator
 if (target.classList.contains('all-clear')) {
-    Calculator_Reset();
-    Update_Display();
+    calculatorReset();
+    updateDisplay();
     return;
 }
 
-Input_Digit(target.value);
-Update_Display();
+inputDigit(target.value);
+updateDisplay();
 })
